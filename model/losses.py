c02bfa23c12834024e42d5c28c4ebfd3bd6e1be7
aruie <amazinghell@naver.com>
2020-08-20 15:24:26 +0900

    5th

--- model/losses.py
+++ model/losses.py
@@ -131,7 +131,7 @@ class FocalLoss(nn.Module) :
                 )
                 regression_losses.append(regression_loss.mean())
             else:
-                regression_losses.append(torch.tensor(0).float()).to(self.device)
+                regression_losses.append(torch.tensor(0).float().to(self.device))
 
         return torch.stack(classification_losses).mean(dim=0, keepdim=True), torch.stack(regression_losses).mean(dim=0, keepdim=True)
 
